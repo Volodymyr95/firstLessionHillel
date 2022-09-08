673e951208ce261a5d8cbfc9fd15e43c04cab570
volodymyr_krokhmaliuk <krokhmalyuk777@gmail.com>
2022-09-08 20:29:27 +0300

    initial commit -m

---
+++ src/it/hiell/Main1.java
@@ -0,0 +1,14 @@
+package it.hiell;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+}
+
+
