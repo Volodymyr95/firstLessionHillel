5a9662a62582b6568446779e678742cd2ccd5f82
volodymyr_krokhmaliuk <krokhmalyuk777@gmail.com>
2022-09-08 20:37:55 +0300

    remove sout lines

--- src/it/hiell/Main1.java
+++ src/it/hiell/Main1.java
@@ -4,10 +4,6 @@ public class Main1 {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
     }
 }
 
